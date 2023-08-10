6548297a003c74dc849005db5535289abf8e8f6e
Anna-Rayevskaya <89201707419sh@gmail.com>
2023-08-10 14:52:13 +0300

    fix4

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,3 @@
 import '../scss/style.scss'
-
 import './main'
+// import './cdn.jsdelivr.net_npm_swiper@10.0.4_swiper-bundle.min.js'
--- src/js/main.js
+++ src/js/main.js
@@ -1,70 +1,75 @@
-import './cdn.jsdelivr.net_npm_swiper@10.0.4_swiper-bundle.min.js'
-const options = {
-  speed: 1000,
-  loop: true,
-  updateOnWindowResize: true,
+// import './cdn.jsdelivr.net_npm_swiper@10.0.4_swiper-bundle.min.js'
+// console.log('text')
+// const options = {
+//   speed: 1000,
+//   loop: true,
+//   updateOnWindowResize: true,
 
-  breakpoints: {
-    0: {
-      slidesPerView: 1,
-      enabled: true
-    },
-    320: {
-      slidesPerView: 1.3,
-      enabled: true
-    },
-    360: {
-      slidesPerView: 1.47,
-      enabled: true
-    },
-    390: {
-      slidesPerView: 1.6,
-      enabled: true
-    },
-    420: {
-      slidesPerView: 2,
-      enabled: true
-    },
-    768: {
-      enabled: false
-    }
-  },
+//   breakpoints: {
+//     0: {
+//       slidesPerView: 1,
+//       enabled: true
+//     },
+//     320: {
+//       slidesPerView: 1.3,
+//       enabled: true
+//     },
+//     360: {
+//       slidesPerView: 1.47,
+//       enabled: true
+//     },
+//     390: {
+//       slidesPerView: 1.6,
+//       enabled: true
+//     },
+//     420: {
+//       slidesPerView: 2,
+//       enabled: true
+//     },
+//     768: {
+//       enabled: false
+//     }
+//   },
 
-  pagination: {
-    el: '.swiper-pagination'
-  }
-}
+//   pagination: {
+//     el: '.swiper-pagination'
+//   }
+// }
 
-let swiper = new Swiper('.swiper', options)
+// let swiper = new Swiper('.swiper', options)
 
-let buttonMore = document.querySelector('.button-read-more')
+let buttonMore = document.querySelector('.button-read-more--brends')
 
 function handleButtonClick() {
-  let buttonMoreIcom = buttonMore.querySelector('.button-read-more__icon')
-  let buttonMoreText = buttonMore.querySelector('.button-read-more__text')
+  let buttonMoreIcon = buttonMore.querySelector(
+    '.button-read-more__icon--brends'
+  )
+  let buttonMoreText = buttonMore.querySelector(
+    '.button-read-more__text--brends'
+  )
   let buttonsMenuTablet = document.querySelectorAll('[data-hidden="tablet"]')
   let buttonsMenuDesktop = document.querySelectorAll('[data-hidden="desktop"]')
 
   buttonsMenuTablet.forEach((element) => {
-    element.classList.toggle('button-menu-brands--hidden')
+    element.classList.toggle('a-menu-brands--hidden')
   })
 
   buttonsMenuDesktop.forEach((element) => {
-    element.classList.toggle('button-menu-brands--hiddenDesktop')
+    element.classList.toggle('a-menu-brands--hiddenDesktop')
   })
 
-  if (buttonMoreIcom.classList.contains('button-read-more__icon--hidden')) {
+  if (buttonMoreIcon.classList.contains('button-read-more__icon--hidden')) {
     buttonMoreText.textContent = 'Показать все'
-    buttonMoreIcom.classList.remove('button-read-more__icon--hidden')
+    buttonMoreIcon.classList.remove('button-read-more__icon--hidden')
   } else {
     buttonMoreText.textContent = 'Скрыть'
-    buttonMoreIcom.classList.add('button-read-more__icon--hidden')
+    buttonMoreIcon.classList.add('button-read-more__icon--hidden')
   }
 }
 
 buttonMore.addEventListener('click', handleButtonClick)
 
-window.addEventListener('orientationchange', () => {
-  swiper.destroy()
-  swiper = new Swiper('.swiper', options)
-})
+// window.addEventListener('orientationchange', () => {
+//   swiper.destroy()
+//   swiper = new Swiper('.swiper', options)
+// })
